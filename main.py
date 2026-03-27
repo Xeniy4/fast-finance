45208160989f265f9c194d83d100675cfb3209dc
Goryaeva Kseniya <goryaevakss@gmail.com>
2026-03-27 21:06:01 +0300

    добавлены тестовые методы

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from email.policy import default
-# from http.client import HTTPException
-
 from fastapi import FastAPI, HTTPException
+from models.operations_models import OperationRequest, CreateWalletRequest
 
 """
 запуск сервера через команду в терминале:
@@ -38,16 +36,71 @@ def get_balance(wallet_name: str | None = None):
 
 
 # В этом методе имя кошелька(wallet_name) передается в path-параметре
-@app.post("/wallets/{name}")
-def receive_money(name: str, amount: int): # receive - получить
-    # Если кошелька с таким именем еще нет - создаем его с балансом 0
-    if name not in BALANCE:
-        BALANCE[name] = 0 # создали кошелек с балансом 0
-    # Если кошелек существует - добавляем сумму к балансу кошелька
-    BALANCE[name] += amount
-    # Возвращаем информацию об операции
+@app.post("/wallets")
+def create_wallet(wallet: CreateWalletRequest):
+    # Проверяем не существует ли такой же кошелек
+    if wallet.name in BALANCE:
+        raise HTTPException(
+            status_code=400,
+            detail= f"Wallet '{wallet.name}' already exist"
+        )
+    # Если не существует, то создаем новый с начальным балансом
+    BALANCE[wallet.name] = wallet.initial_balance
+    # Возвращаем инфу о созданном кошельке
     return {
-        "message": f"Added {amount} to {name}",
-        "wallet": name,
-        "new_balance": BALANCE[name]
+      "message": f"Wallet '{wallet.name}' created",
+      "wallet": wallet.name,
+      "new_balance": BALANCE[wallet.name]
+    }
+
+
+@app.post("/operations/income") # income - доход
+def add_incomes(operation: OperationRequest):
+    # Проверить, существует ли кошелек
+    if operation.wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{operation.wallet_name}' not found"
+        )
+
+    # Добавить доход к балансу
+    BALANCE[operation.wallet_name] += operation.amount
+
+    # Возвратить информацию об операции
+    return {
+      "message": "Income added",
+      "wallet": operation.wallet_name,
+      "amount": operation.amount,
+      "description": operation.descriptions,
+      "new_balance": BALANCE[operation.wallet_name]
+    }
+
+
+@app.post("/operations/expense") # expense - расход
+def add_expense(operation: OperationRequest):
+    # Проверить, существует ли кошелек
+    if operation.wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{operation.wallet_name}' not found"
+        )
+    # Проверить достаточно ли средств в кошельке
+    if BALANCE[operation.wallet_name] < operation.amount:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Insufficient funds. "
+                   f"Available: {BALANCE[operation.wallet_name]}" # Недостаточно средств. Доступно:
+        )
+
+
+    # Вычесть расход из баланса
+    BALANCE[operation.wallet_name] -= operation.amount
+
+    # Возвратить информацию об операции
+    return {
+      "message": "Expense added",
+      "wallet": operation.wallet_name,
+      "amount": operation.amount,
+      "description": operation.descriptions,
+      "new_balance": BALANCE[operation.wallet_name]
     }
---
+++ models/operations_models.py
@@ -0,0 +1,52 @@
+from http.server import ThreadingHTTPServer
+
+from pydantic import BaseModel, Field, field_validator
+
+
+class OperationRequest(BaseModel):
+    wallet_name: str = Field(..., max_length=127)  # ... (называется Ellipsis) - значит поле обязательно в заполнении)
+    amount: float
+    descriptions: str | None = Field(None, max_length=255)  # поле не обязательно к заполнению и дефолтное значение None
+
+    # Валидация, что поле положительное
+    @field_validator('amount')
+    def amount_must_be_positive(cls, value: float) -> float:
+        # Проверить, что значение больше 0
+        if value <= 0:
+            raise ValueError("Amount must be positive")
+        # Вернуть значение
+        return value
+
+
+    # Удаление лишних пробелов по бокам
+    @field_validator('wallet_name')
+    def wallet_name_not_empty(cls, value: str) -> str:
+        # Убрать пробелы побокам
+        value = value.strip()
+        # Убедиться, что строка не пустая
+        if not value:
+            raise ValueError("Wallet name cannot be empty")
+
+        # Вернуть значение
+        return value
+
+
+class CreateWalletRequest(BaseModel):
+    name: str = Field(..., max_length=127)
+    initial_balance: float = 0
+
+
+    # Удаление лишних пробелов по бокам
+    @field_validator('name')
+    def name_not_empty(cls, value: str) -> str:
+        value = value.strip()
+        if not value:
+            raise ValueError("Name cannot be empty")
+        return value
+
+    # Валидация, что баланс не отрицательный
+    @field_validator('initial_balance')
+    def balance_not_negative(cls, value: float) -> float:
+        if value < 0:
+            raise ValueError("initial balance cannot be negative")
+        return value
