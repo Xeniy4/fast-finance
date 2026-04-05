6ba10a15ec4ae238560654ae1c3f5ccd68f5d74c
Goryaeva Kseniya <goryaevakss@gmail.com>
2026-04-05 16:53:55 +0300

    Добавлена простая авторизация по логину и поиск юзера

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.dependency import get_db
+from app.database_models import User
+from app.dependency import get_db, get_current_user
 from app.service import users as users_service
 
 from app.schemas import UsersRequest, UsersResponse
@@ -12,3 +13,9 @@ router = APIRouter()
 @router.post("/users", response_model=UsersResponse)
 def create_user(payload: UsersRequest, db: Session = Depends(get_db)):
     return users_service.create_user(db=db, login=payload.login)
+
+
+
+@router.get("/users/me", response_model=UsersResponse)
+def get_current_user(current_user: User = Depends(get_current_user)):
+    return  UsersResponse.model_validate(current_user)
--- app/dependency.py
+++ app/dependency.py
@@ -2,15 +2,26 @@
 Тут содержатся зависимости. Например, для создания подключения к БД.
 Работает отдаленно как фикстуры. Только тут код выполняется внутри,
 а выполнение до и после указывается в функции зависимости
-
+далее
+Sequrity object - это структура, которая описывает какой тип авторизации описывает для доступа к ресурсу.
+Создают, чтобы четко определить правила доступа: токены, ключи и т.п.
 """
 from typing import Generator
 
+from fastapi import HTTPException
+from fastapi.params import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
+from app.database_models import User
+from app.repository import users as users_repository
+
+sequrity = HTTPBearer()
 
 
+
+#Функция для получения сессии БД через dependency injection в FastApi
 def get_db() -> Generator[Session, None, None]:
     # происходит подключение к бд
     db = SessionLocal()
@@ -18,3 +29,14 @@ def get_db() -> Generator[Session, None, None]:
         yield db # весь код функции должен выполняться здесь
     finally:
         db.close() # бд отключается
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(sequrity),
+                     db: Session = Depends(get_db)) -> User:
+    login = credentials.credentials
+    user = users_repository.get_user(db=db,login=login)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Unauthorised")
+
+    return user
