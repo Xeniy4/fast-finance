3329554a476469474c10ceb84b980f2e95f83b32
Goryaeva Kseniya <goryaevakss@gmail.com>
2026-04-07 13:28:25 +0300

    добавлен негативнфй тест, дополнен README.md

--- tests/test_api/test_operation.py
+++ tests/test_api/test_operation.py
@@ -5,7 +5,19 @@ from tests.helpers.endpoints import Endpoints
 
 from tests.helpers.data_tests import gen_random_amount, get_random_name, gen_random_str
 
+
 def test_add_expense_success(db_session, client):
+    """
+    Проверка возможности списания средств
+    Предусловия:
+        Создать тестовые данные (логин, начальный баланс, сумма, описание, имя кошелька)
+    Шаги:
+        1. Создать юзера и добавить в БД
+        2. Создать кошелек и добавить в БД
+        3. Отправить post запрос на эндпоинт "/api/v1/operations/expense" для списания суммы
+        4. Проверить данных в ответе
+    ОР: Данные ответа соответствуют данным из запроса, сумма в балансе обновилась
+    """
     # Arrange
     user_login = get_random_name()
     balance = gen_random_amount(200, 400)
@@ -22,7 +34,7 @@ def test_add_expense_success(db_session, client):
     db_session.commit()
     db_session.refresh(instance=wallet)
 
-    #Act
+    # Act
     response = client.post(
         url=Endpoints.operations_expense.value,
         json={  # через модель OperationRequest не получилось реализовать, тк у json проблемы с типом Decimal
@@ -32,7 +44,6 @@ def test_add_expense_success(db_session, client):
         },
         headers={"Authorization": f"Bearer {user.login}"}
     )
-
 
     # Assert
     assert response.status_code == http.HTTPStatus.OK
@@ -44,7 +55,26 @@ def test_add_expense_success(db_session, client):
 
 
 def test_add_expense_negative_amount(db_session, client):
+    """
+    Проверка отсутствия возможности списания отрицательной суммы из кошелька
+    Предусловия:
+        Создать тестовые данные (логин, начальный баланс, сумма, описание, имя кошелька)
+    Шаги:
+        1. Создать юзера и добавить в БД
+        2. Создать кошелек и добавить в БД
+        3. Отправить post запрос на эндпоинт "/api/v1/operations/expense" для списания суммы.
+            В теле запроса отправить отрицательное значение
+        4. Проверить данных в ответе
+    ОР: Данные ответа соответствуют данным из запроса, сумма в балансе обновилась
+    """
     # Arrange
+    user_login = get_random_name()
+    balance = gen_random_amount(100, 199)
+    wallet_name = get_random_name()
+    descriptions = gen_random_str()
+    amount = gen_random_amount(200, 300)
+    expect_balance = balance - amount
+
     user = User(login="test")
     db_session.add(user)
     db_session.flush()
@@ -54,28 +84,15 @@ def test_add_expense_negative_amount(db_session, client):
     db_session.commit()
     db_session.refresh(instance=wallet)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Act
+    response = client.post(
+        url=Endpoints.operations_expense.value,
+        json={
+            "wallet_name": wallet.name,
+            "amount": amount,
+            "descriptions": descriptions
+        },
+        headers={"Authorization": f"Bearer {user.login}"}
+    )
+    assert response.status_code == http.HTTPStatus.BAD_REQUEST
 
